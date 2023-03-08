476b3a040d5876c8161d8668ae68d078e0afa1e5
Wellington <wellington-158@hotmail.com>
2023-03-08 12:10:05 -0300

    :construction:feat: Checkout page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,18 +2,22 @@ import * as Styles from "./styles"
 import { MapPin } from "phosphor-react"
 import logo from "~/assets/illustration/logo.svg"
 import { ShippingCart } from "../ShippingCart"
+import { Link } from "react-router-dom"
 
 export function Header() {
 	return (
 		<Styles.HeaderContainer>
-			<img src={logo} alt="Logo da marca com um texto de Coffee Delivery" />
-
+			<Link to={"/"}>
+				<img src={logo} alt="Logo da marca com um texto de Coffee Delivery" />
+			</Link>
 			<Styles.ActionsWrapper>
 				<Styles.LocationWrapper>
 					<MapPin size={22} weight="fill" />
 					<p>Vinhedo, SP</p>
 				</Styles.LocationWrapper>
-				<ShippingCart variant="primary" />
+				<Link to={"/checkout"}>
+					<ShippingCart variant="primary" />
+				</Link>
 			</Styles.ActionsWrapper>
 		</Styles.HeaderContainer>
 	)
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,20 @@ import { Layout } from "~/layout"
 import { createBrowserRouter } from "react-router-dom"
 import { ErrorBoundary } from "~/pages/ErrorBoundary"
 import { Home } from "~/pages/Home"
+import { Checkout } from "~/pages/Checkout"
 
 export const router = createBrowserRouter([
 	{
 		path: "/",
 		element: <Layout />,
 		errorElement: <ErrorBoundary />,
-		children: [{ index: true, element: <Home />, errorElement: <ErrorBoundary /> }],
+		children: [
+			{ index: true, element: <Home />, errorElement: <ErrorBoundary /> },
+			{
+				element: <Checkout />,
+				errorElement: <ErrorBoundary />,
+				path: "checkout",
+			},
+		],
 	},
 ])
