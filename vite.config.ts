80fca22e06bde5a9aa1c65e08ea8751870c78a7d
Wellington <wellington-158@hotmail.com>
2024-04-27 16:46:37 -0300

    chore: ajuste nas configurações de teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
+///<reference types="vitest" />
+///<reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
+	test: {
+		environment: 'jsdom',
+		globals: true,
+	},
 })
